2da9b385a8cdee7be5dfd6263e6b78c46591f4e5
Li Dongjin <12110321@mail.sustech.edu.cn>
2023-06-29 18:56:29 +0800

    修改geom.py

--- src/geom.py
+++ src/geom.py
@@ -1,7 +1,7 @@
 import math
 
 class BasePoint():
-    def __init__(self, x = 0, y = 0, rad = 0)
+    def __init__(self, x = 0, y = 0, rad = 0):
         self.x = x
         self.y = y
         self.rad = rad
@@ -9,7 +9,7 @@ class BasePoint():
     def absolute(self):
         return self.x, self.y, self.rad
 
-    def relative_ab(self):
+    def relative_ab(self, point):
         ori_x, ori_y, ori_rad = point.absolute()
         rel_x = self.x - ori_x
         rel_y = self.y - ori_y
@@ -29,7 +29,7 @@ class Point():
             self.base = base
 
     def absolute(self):
-        x, y, rad = base.absolute()
+        x, y, rad = self.base.absolute()
         rad = rad + self.rad
         x = x + self.distance * math.cos(rad)
         y = y + self.distance * math.sin(rad)
@@ -63,11 +63,18 @@ class Point():
 class Shape():
     def cross(self):
         return False
+    
+    def square(self):
+        return None, None
 
 class Circle(Shape):
     def __init__(self, center, rad):
         self.center = center
         self.rad = rad
+
+    def square(self):
+        x, y, rad = self.center.absolute()
+        return (x-self.rad, y-self.rad), (x+self.rad, y+self.rad)
 
     def cross(self, shape):
         if isinstance(shape, Circle):
@@ -77,7 +84,7 @@ class Circle(Shape):
         else:
             return shape.cross(self)
 
-    def in(self, point):
+    def point_in(self, point):
         x, y, r = point.relative_ab(self.center)
         return x^2 + y^2 < self.rad^2
 
@@ -85,6 +92,23 @@ class Line(Shape):
     def __init__(self, point1, point2):
         self.point1 = point1
         self.point2 = point2
+
+    def square(self):
+        x0, y0, r = self.point1.absolute()
+        x1, y1, r = self.point2.absolute()
+        if x0 < x1:
+            x_min = x0
+            x_max = x1
+        else:
+            x_min = x1
+            x_max = x0
+        if y0 < y1:
+            y_min = y0
+            y_max = y1
+        else:
+            y_min = y1
+            y_max = y0
+        return (x_min, y_min), (x_max, y_max)
     
     def cross(self, shape):
         if isinstance(shape, Line):
@@ -107,9 +131,9 @@ class Line(Shape):
                     else:
                         return (y0<mesure & mesure<0)
         elif isinstance(shape, Circle):
-            if shape.in(self.point1):
+            if shape.point_in(self.point1):
                 return True
-            if shape.in(self.point2):
+            if shape.point_in(self.point2):
                 return True
             x0, y0, r = self.point1.relative_ab(shape.center)
             x1, y1, r = self.point2.relative_ab(shape.center)
@@ -119,8 +143,168 @@ class Line(Shape):
         else:
             return shape.cross(self)
 
-class Triangle(Shape):
-    def __init__(self, point1, point2, point3):
-        self.point1 = point1
-        self.point2 = point2
-        self.point3 = point3
+class Polygon(Shape):
+    def __init__(self, pointlist):
+        self.pointlist = pointlist
+
+    def center(self):
+        x = 0
+        y = 0
+        for point in self.pointlist:
+            x += point.x
+            y += point.y
+        return x/len(self.pointlist), y/len(self.pointlist)
+
+    def point_in(self, point):
+        point = BasePoint(*point.absolute())
+        pointlist = [BasePoint(*point.absolute()) for point in self.pointlist]
+        values = []
+        for i in range(len(self.pointlist)):
+            vec1 = (pointlist[i].x - point.x, pointlist[i].y - point.y)
+            vec2 = (pointlist[i-1].x - pointlist[i].x, pointlist[i-1].y - pointlist[i].y)
+            values.append(vec1[0]*vec2[1] - vec1[1]*vec2[0])
+        if all([value > 0 for value in values]) or all([value < 0 for value in values]):
+            return True
+        else:
+            return False
+
+    def square(self):
+        x_min = 0
+        x_max = 0
+        y_min = 0
+        y_max = 0
+        for i in range(len(self.pointlist)):
+            x, y, r = self.pointlist[i].absolute()
+            if x < x_min:
+                x_min = x
+            if x > x_max:
+                x_max = x
+            if y < y_min:
+                y_min = y
+            if y > y_max:
+                y_max = y
+        return (x_min, y_min), (x_max, y_max)
+
+    def cross(self, shape):
+        square_min, square_max = self.square()
+        square_min1, square_max1 = shape.square()
+        if square_min[0] > square_max1[0] or square_min1[0] > square_max[0]:
+            return False
+        if square_min[1] > square_max1[1] or square_min1[1] > square_max[1]:
+            return False
+        if isinstance(shape, Circle):
+            for i in range(len(self.pointlist)):
+                if shape.point_in(self.pointlist[i]):
+                    return True
+            for i in range(len(self.pointlist)):
+                line = Line(self.pointlist[i-1], self.pointlist[i])
+                if line.cross(shape):
+                    return True
+            if self.point_in(shape.center):
+                return True
+            return False
+        elif isinstance(shape, Line):
+            for i in range(len(self.pointlist)):
+                line = Line(self.pointlist[i-1], self.pointlist[i])
+                if line.cross(shape):
+                    return True
+            return False
+        elif isinstance(shape, Polygon):
+            center = BasePoint(*self.center())
+            if shape.point_in(center):
+                return True
+            for i in range(len(self.pointlist)):
+                for j in range(len(shape.pointlist)):
+                    line = Line(self.pointlist[i-1], self.pointlist[i])
+                    if line.cross(Line(shape.pointlist[j-1], shape.pointlist[j])):
+                        return True
+            return False
+        else:
+            return shape.cross(self)
+        
+class Sector(Shape):
+    def __init__(self, point, rad, angle1, angle2):
+        self.point = point
+        self.rad = rad
+        self.angle1 = angle1
+        self.angle2 = angle2
+
+    def lines(self):
+        x, y, r = self.point.absolute()
+        x1 = x + self.rad*math.cos(self.angle1)
+        y1 = y + self.rad*math.sin(self.angle1)
+        x2 = x + self.rad*math.cos(self.angle2)
+        y2 = y + self.rad*math.sin(self.angle2)
+        return Line(self.point, BasePoint(x1, y1)), Line(self.point, BasePoint(x2, y2))
+
+    def point_in(self, point):
+        x, y, r = point.absolute()
+        if x^2 + y^2 > self.rad^2:
+            return False
+        angle = math.atan2(y-self.point.absolute()[1], x-self.point.absolute()[0])
+        if self.angle1 < self.angle2:
+            if self.angle1 < angle & angle < self.angle2:
+                return True
+            else:
+                return False
+        else:
+            if self.angle1 < angle | angle < self.angle2:
+                return True
+            else:
+                return False
+            
+    def square(self):
+        x, y, r = self.point.absolute()
+        x_min = x - self.rad
+        x_max = x + self.rad
+        y_min = y - self.rad
+        y_max = y + self.rad
+        return (x_min, y_min), (x_max, y_max)
+    
+    def cross(self, shape):
+        square_min, square_max = self.square()
+        square_min1, square_max1 = shape.square()
+        if square_min[0] > square_max1[0] or square_min1[0] > square_max[0]:
+            return False
+        if square_min[1] > square_max1[1] or square_min1[1] > square_max[1]:
+            return False
+        if isinstance(shape, Circle):
+            line1, line2 = self.lines()
+            if line1.cross(shape):
+                return True
+            if line2.cross(shape):
+                return True
+            if not shape.cross(Circle(self.point, self.rad)):
+                return False
+            x, y, r = shape.center.absolute()
+            angle = math.atan2(y-self.point.absolute()[1], x-self.point.absolute()[0])
+            if self.angle1 < self.angle2:
+                if self.angle1 < angle & angle < self.angle2:
+                    return True
+                else:
+                    return False
+            else:
+                if self.angle1 < angle | angle < self.angle2:
+                    return True
+                else:
+                    return False
+        elif isinstance(shape, Line):
+            line1, line2 = self.lines()
+            if line1.cross(shape):
+                return True
+            if line2.cross(shape):
+                return True
+            if self.point_in(shape.point1) | self.point_in(shape.point2):
+                return True
+            return False
+        elif isinstance(shape, Polygon):
+            line1, line2 = self.lines()
+            for i in range(len(shape.pointlist)):
+                if self.point_in(shape.pointlist[i]):
+                    return True
+                if line1.cross(Line(shape.pointlist[i-1], shape.pointlist[i])):
+                    return True
+                if line2.cross(Line(shape.pointlist[i-1], shape.pointlist[i])):
+                    return True
+                
+            
