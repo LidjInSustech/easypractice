7fa2a72a467d876a42c474b27e9ecc99e41b6423
Felix Li(Li Dongjin) <flametrel@foxmail.com>
2023-07-21 17:49:23 +0800

    重写util.py

--- src/util.py
+++ src/util.py
@@ -3,22 +3,20 @@ import os
 import pygame as pg
 
 config = None
-language = None
+trans_table = None
 loading_image = None
 
 def init():
-    global font, config, language
+    global config, trans_table
     config = read_config('config.json')
-    if config['language'] == 'en':
-        language = {}
-    else:
-        with open('data/lang/{}.json'.format(config['language']), 'r') as f:
-            language = json.load(f)
+    with open('data/lang/{}.json'.format(config['language']), 'r') as f:
+        trans_table = json.load(f)
 
 def get_word(text):
-    global language
-    if text in language.keys():
-        return language[text]
+    global trans_table
+    words = trans_table['words']
+    if text in words.keys():
+        return words[text]
     else:
         return text
 
@@ -40,99 +38,104 @@ def get_font(size):
     except:
         return pg.font.SysFont('Calibri', size)
 
-def load_image(filename, hight=None, width=None, colorkey=None):
+def load_image(filename, rect = None, colorkey = None):
     filename = os.path.join('res', filename)
+
     try:
         image = pg.image.load(filename)
     except pg.error:
         print('Cannot load image:', filename)
         raise SystemExit(str(get_word('Could not load image')))
         return None
+
+    if rect is not None:
+        image = pg.transform.scale(image, rect.size)
+
     image = image.convert()
+
     if colorkey is not None:
         if colorkey == -1:
             colorkey = image.get_at((0,0))
         image.set_colorkey(colorkey)
-    if hight is not None and width is not None:
-        image = pg.transform.scale(image, (hight, width))
+
     return image
 
-def load_image_alpha(filename, hight=None, width=None):
+def load_image_alpha(filename, rect = None):
     filename = os.path.join('res', filename)
+
     try:
         image = pg.image.load(filename)
     except pg.error:
         print('Cannot load image:', filename)
         raise SystemExit(str(get_word('Could not load image')))
         return None
-    image = image.convert_alpha()
-    if hight is not None and width is not None:
-        image = pg.transform.scale(image, (hight, width))
-    return image
 
-def loading_page():
+    if rect is not None:
+        image = pg.transform.scale(image, rect.size)
+
+    return image.convert_alpha()
+
+def show_loading_page():
     global loading_image
     if loading_image == None:
-        loading_image = load_image('basic/loading_page.png', pg.display.get_surface().get_width(), pg.display.get_surface().get_height())
-        if loading_image == None:
-            loading_image = pg.Surface((pg.display.get_surface().get_width(), pg.display.get_surface().get_height()))
+        loading_image = load_image('basic/loading_page.png', pg.display.get_surface().get_rect())
         font = get_font(32)
-        #loading_image = font.render(get_word('loading...'), True, (50, 100, 150))
         loading_image.blit(font.render(get_word('loading...'), True, (50, 100, 150)), (20,20))
     pg.display.get_surface().blit(loading_image, (0,0))
     pg.display.update()
 
 
 class Button():
-    def __init__(self, text, rect):
-        global language
-        if text in language.keys():
-            text = language[text]
+    def __init__(self, text, images):
+        global trans_table
+        if text in trans_table.keys():
+            text = trans_table[text]
         self.text = text
-        self.rect = rect
+        rect = images[0].get_rect()
+
         font = get_font(int(rect.height/2))
         font = font.render(text, True, (251,254,110), (0,0,0))
         font.set_colorkey((0,0,0))
         font_rect = font.get_rect()
-        
-        self.up_image = load_image_alpha('basic/button_up.png', rect.width, rect.height)
-        if self.up_image is None:
-            self.up_image = pg.Surface((rect.width, rect.height))
-            self.up_image.fill((64,32,16))
-        font_rect.center = self.up_image.get_rect().center
-        self.up_image.blit(font, font_rect)
-
-        self.down_image = load_image_alpha('basic/button_down.png', rect.width, rect.height)
-        if self.down_image is None:
-            self.down_image = pg.Surface((rect.width, rect.height))
-            self.down_image.fill((32,64,128))
-        self.down_image.blit(font, font_rect)
+        font_rect.center = rect.center
+        self.up = images[0].copy()
+        self.up.blit(font, font_rect)
+        self.down = images[1].copy()
+        self.down.blit(font, font_rect)
 
 class Button_Box():
-    def __init__(self, rect, button_names, picture=None):
+    def __init__(self, rect, button_names, picture=None, margin=10):
         self.screen = pg.display.get_surface()
         self.rect = rect
         self.curser = 0
-        self.load_buttons(button_names)
+        self.load_buttons(button_names, margin)
         self.picture = picture
+        self.button_names = button_names
+        self.margin = margin
+
+    def load_buttons(self, button_names, margin):
+        button_width = (self.rect.width - margin) - margin
+        button_height = (self.rect.height - margin)//len(button_names) - margin
+        button_rect = pg.Rect(0, 0, button_width, button_height)
+
+        self.up_image = load_image_alpha('basic/button_up.png', button_rect)
+        self.down_image = load_image_alpha('basic/button_down.png', button_rect)
+        self.button_rect = button_rect
+        self.buttons = [Button(text, (self.up_image, self.down_image)) for text in button_names]
 
     def draw(self):
-        self.screen.fill((0,0,0))
-        if self.picture is not None:
+        if self.picture is None:
+            self.screen.fill((0,0,0))
+        else:
             self.screen.blit(self.picture, self.picture.get_rect())
-        for button in self.buttons:
-            self.screen.blit(button.up_image, button.rect)
-        self.screen.blit(self.buttons[self.curser].down_image, self.buttons[self.curser].rect)
+
+        for i in range(self.curser):
+            self.screen.blit(self.buttons[i].up, (self.rect.x, self.rect.y+self.margin + i*(self.margin+self.button_rect.h)))
+        self.screen.blit(self.buttons[self.curser].down, (self.rect.x, self.rect.y+self.margin + self.curser*(self.margin+self.button_rect.h)))
+        for i in range(self.curser+1, len(self.buttons)):
+            self.screen.blit(self.buttons[i].up, (self.rect.x, self.rect.y+self.margin + i*(self.margin+self.button_rect.h)))
+
         pg.display.update()
-
-    def load_buttons(self, button_names):
-        margin = 10
-        width = (self.rect.width - margin)//2 - margin
-        height = (self.rect.height - margin)//len(button_names) - margin
-
-        self.buttons = []
-        for row in range(len(button_names)):
-            self.buttons.append(Button(button_names[row], pg.Rect(margin, margin + (height + margin) * row, width, height)))
 
     def start(self):
         self.running = True
@@ -157,46 +160,48 @@ class Button_Box():
                         return self.curser
 
 class Rolling_Box(Button_Box):
-    def __init__(self, rect, button_rect, button_names, picture=None):
-        self.margin = 10
+    def __init__(self, rect, button_rect, button_names, picture=None, margin=10):
+        self.margin = margin
         self.screen = pg.display.get_surface()
-        self.ori_screen = pg.Surface((button_rect.width, (button_rect.height+self.margin)*len(button_names)), flags=pg.SRCALPHA)
+        self.rolling_rect = pg.Rect(0, 0, button_rect.w, (button_rect.h+margin)*len(button_names))
+        rect.h = min(rect.h, self.rolling_rect.height)
         self.rect = rect
         self.button_rect = button_rect
         self.curser = 0
         self.load_buttons(button_names)
         self.picture = picture
+        self.offset = (self.rolling_rect.height - self.rect.height) / (len(self.buttons) - 1)
 
     def load_buttons(self, button_names):
-        margin = self.margin
-        width = self.button_rect.width
-        height = self.button_rect.height
-        self.buttons = []
-        for row in range(len(button_names)):
-            self.buttons.append(Button(button_names[row], pg.Rect(margin, margin + (height + margin) * row, width, height)))
-        for button in self.buttons:
-            self.ori_screen.blit(button.up_image, button.rect)
+        self.up_image = load_image_alpha('basic/button_up.png', self.button_rect)
+        self.down_image = load_image_alpha('basic/button_down.png', self.button_rect)
+        self.buttons = [Button(text, (self.up_image, self.down_image)) for text in button_names]
 
     def draw(self):
-        self.screen.fill((0,0,0))
-        if self.picture is not None:
+        if self.picture is None:
+            self.screen.fill((0,0,0))
+        else:
             self.screen.blit(self.picture, self.picture.get_rect())
-        temp = self.ori_screen.copy()
-        temp.blit(self.buttons[self.curser].down_image, self.buttons[self.curser].rect)
-        gap = (self.ori_screen.get_height() - self.rect.height) / (len(self.buttons) - 1)
-        self.screen.blit(temp, self.rect, area=(0, gap*self.curser, self.rect.width, self.rect.height))
+
+        rolling_area = pg.Surface(self.rolling_rect.size, flags=pg.SRCALPHA)
+
+        for i in range(self.curser):
+            rolling_area.blit(self.buttons[i].up, (0, self.margin + i*(self.margin+self.button_rect.h)))
+        rolling_area.blit(self.buttons[self.curser].down, (0, self.margin + self.curser*(self.margin+self.button_rect.h)))
+        for i in range(self.curser+1, len(self.buttons)):
+            rolling_area.blit(self.buttons[i].up, (0, self.margin + i*(self.margin+self.button_rect.h)))
+        start_pos = (self.rolling_rect.w, self.curser*(self.margin+self.button_rect.h))
+        end_pos = (self.rolling_rect.w, start_pos[1] + self.margin*2 + self.button_rect.h)
+        pg.draw.line(rolling_area, (251,254,110), start_pos, end_pos, self.margin)
+        
+        self.screen.blit(rolling_area, self.rect, area=self.rect.move(0, self.offset*self.curser))
         pg.display.update()
 
-
-class Menu_Page(Button_Box):#deprecated
-    def __init__(self, button_names, picture, picture_rect):
-        rect = pg.display.get_surface().get_rect()
-        rect.width = rect.width // 2
-        super().__init__(rect, button_names)
-        self.picture = picture
-        self.picture_rect = picture_rect
-
-    def draw(self):
-        self.screen.fill((0,0,0))
-        self.screen.blit(self.picture, self.picture_rect)
-        super().draw()
+def load_sound(filename):
+    class NoneSound:
+        def play(self):
+            pass
+    if not pg.mixer or not pg.mixer.get_init():
+        return NoneSound()
+    sound = pg.mixer.Sound(filemane)
+    return sound
